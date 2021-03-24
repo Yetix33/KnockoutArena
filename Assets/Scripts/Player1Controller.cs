5aaff6d9806ec09dd5c8a2c0cffd1669b9aa7ba7
Yetix33 <apate388@uwo.ca>
2021-03-24 18:07:25 -0400

    initial

---
+++ Assets/Scripts/Player1Controller.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Controller : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody r_body;
+    private Vector3 _inputs = Vector3.zero;
+    private Animator animator;
+    private bool isKnockback = false; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        r_body = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("p1_horizontal");
+        float z = Input.GetAxis("p1_vertical");
+
+        if(Input.GetButtonDown("p1_block")){
+            r_body.velocity = Vector3.zero;
+            r_body.angularVelocity = Vector3.zero;
+            r_body.Sleep();
+            animator.ResetTrigger("walk");
+            animator.SetTrigger("block");
+        }
+        if((x != 0 || z != 0) && !isKnockback && !animator.GetCurrentAnimatorStateInfo(0).IsName("block")){
+            r_body.velocity = new Vector3(x*speed, r_body.velocity.y, z * speed);
+            Vector3 movementDirection = new Vector3(x,0,z);
+            transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (movementDirection), Time.deltaTime *20f);
+            //transform.rotation = Quaternion.LookRotation(movementDirection);
+            animator.SetTrigger("walk");
+        } else if(animator.GetCurrentAnimatorStateInfo(0).IsName("walk")) {
+            animator.SetTrigger("stop_walk");
+        }
+
+        if(Input.GetButtonDown("p1_base_attack")&& !isKnockback){
+            animator.SetTrigger("base_attack");
+        }
+       
+        if(r_body.velocity.magnitude <1){
+            isKnockback = false;
+        }
+    }
+
+
+    void OnTriggerEnter(Collider col){
+       if(col.name == "sword"){
+           isKnockback = true;
+       }
+    }
+}
