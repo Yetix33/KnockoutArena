327981db4ca7a4c6d0460291cbb1e486acef61c1
Tariq Hirji <tariqhirji15@gmail.com>
2021-03-28 15:41:33 -0400

    Clean up ignored files

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+   
+    void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
