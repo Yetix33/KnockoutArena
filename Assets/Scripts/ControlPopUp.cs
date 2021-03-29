e13226c0cc3dd121c55d0636b6b748f5f4a70c7f
saviojoseph <savioj12@gmail.com>
2021-03-29 02:48:00 -0400

    Added a Controls Menu
    Controls Menu for players to have a quick glance at the controls

---
+++ Assets/Scripts/ControlPopUp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPopUp : MonoBehaviour
+{
+    public GameObject Panel;
+
+    //On button click, activate text panel 
+        public void openPanel(){
+        if(Panel != null){
+            Panel.SetActive(true);
+        }
+    }
+
+}
