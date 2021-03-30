2a461d60d4c46b16333a6cfc7d92b63bd4ad671c
Tariq Hirji <tariqhirji15@gmail.com>
2021-03-30 16:25:28 -0400

    Added Powerup spawner
    Will still need to handle collision, remove powerup and restart timer for next one

---
+++ Assets/Scripts/Powerups.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerups : MonoBehaviour
+{
+    private int start = 0;
+    private bool TakingTime;
+    private int time;
+
+    private int rangeStart = 5;
+    private int rangeEnd = 15;
+    private int powerupTime;
+    private System.Random randomizer;
+    private int TimeOfNextPowerup;
+    private bool livePowerup;
+
+    public GameObject strengthPowerup;
+    public GameObject speedPowerup;
+    public GameObject blockPowerup;
+
+
+    private string[] powerups = {"strength", "speed", "block"};
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = start;
+        livePowerup = false;
+        randomizer = new System.Random();
+        TimeOfNextPowerup = findNextPowerupTime();
+        Debug.Log(TimeOfNextPowerup);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TakingTime == false) {
+            StartCoroutine(TimeAdd());
+        }
+
+        if (time == TimeOfNextPowerup && livePowerup == false) {
+            AddPowerup();
+        }
+
+
+    }
+
+    // Find random time that next powerup should spawn at
+    private int findNextPowerupTime() {
+        int randTime = randomizer.Next(rangeStart, rangeEnd + 1);
+        return time + randTime;
+    }
+
+    private void AddPowerup() {
+        int index = randomizer.Next(0, 3);
+        string power = powerups[index];
+        Debug.Log(index);
+        livePowerup = true;
+        if (power == "strength") {
+            Instantiate(strengthPowerup);
+            return;
+        } else if (power == "speed") {
+            Instantiate(speedPowerup);
+            return;
+        } else {
+            Instantiate(blockPowerup);
+            return;
+        }  
+    }
+
+    // Coroutine that adds 1 second and waits for next second 
+    IEnumerator TimeAdd() {
+        TakingTime = true;
+        yield return new WaitForSeconds(1);
+        time += 1;
+        // Update time display here if adding!!
+        TakingTime = false;
+    }
+}
